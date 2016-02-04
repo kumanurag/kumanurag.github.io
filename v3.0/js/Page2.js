d50559158cdd23daf2db38a6b7d712909eaeb8ed
kumanurag <Anurag@MacBook-Air.local>
2016-02-05 01:19:15 +0530

    small size image

--- v3.0/js/Page2.js
+++ v3.0/js/Page2.js
@@ -433,17 +433,17 @@ $(document).ready(function () {
 });
     });
 
-$(document).ready(function () {
-           $('.down').mouseenter(function(){
-          $('.down').css('display', 'none');
-          $('.down_hover').css('display', 'block');
-        });
-           $('.down_hover').mouseleave(function(){
-            $('.down_hover').css('display', 'none')
-          $('.down').css('display', 'block');
-          ;
-        });
-});
+// $(document).ready(function () {
+//            $('.down').mouseenter(function(){
+//           $('.down').css('display', 'none');
+//           $('.down_hover').css('display', 'block');
+//         });
+//            $('.down_hover').mouseleave(function(){
+//             $('.down_hover').css('display', 'none')
+//           $('.down').css('display', 'block');
+//           ;
+//         });
+// });
 
 });
 
